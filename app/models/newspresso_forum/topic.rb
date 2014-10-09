f09c7efa35b585b6fd7030be3d6dbee4979de174
Chris DiMartino <chris.dimartino@gmail.com>
2014-10-09 09:58:25 -0400

    Added Topic expiration methods
      - expired?
      - unexpire!
      - topic_expiration (private)

--- app/models/newspresso_forum/topic.rb
+++ app/models/newspresso_forum/topic.rb
@@ -10,6 +10,36 @@ module NewspressoForum
 
     friendly_id :slug_candidates, use: :slugged
 
+    validates_presence_of :body, :title
+
+    #
+    # Tests if the topic is in an expired state
+    #
+    # @return [Bool]
+    def expired?
+      created_at < topic_expiration.days.ago and updated_at < topic_expiration.days.ago
+    end
+
+    #
+    # Update the updated_at to reset the topic expiration date
+    #
+    def unexpire!
+      update_attribute(:updated_at, Time.now)
+    end
+
+    private
+
+    #
+    # Update the slug to add an integer if already exists
+    #
+    def increment_slug
+      self.class.where(title: self.title).count + 1
+    end
+
+    #
+    # Candidates for the slug
+    #
+    # @return [Array] Array of slug candidates
     def slug_candidates
       [
         :title,
@@ -18,10 +48,12 @@ module NewspressoForum
 
     end
 
-    def increment_slug
-      self.class.where(title: self.title).count + 1
+    #
+    # Return the count used for topic expirations
+    #
+    # @return [Integer] Either defined TOPIC_EXPIRATION_DAYS or 14 as default
+    def topic_expiration
+      AppConfig['TOPIC_EXPIRATION_DAYS'] || 14
     end
-
-    validates_presence_of :body, :title
   end
 end
